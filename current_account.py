f26b2c6f23b5c4ed7dda75f8e06e3e1c7bba8cf6
Lewis Kabui <lewis.maina@andela.com>
2016-08-18 15:44:22 +0300

    (feature) Create CurrentAccount class that inherits from BankAccount

---
+++ current_account.py
@@ -0,0 +1,42 @@
+from bank_account import BankAccount
+
+
+class CurrentAccount(BankAccount):
+    """
+    Class representation of a CurrentAccount.
+
+    Attributes:
+        balance: Represents current account balance. Minimum amount is 0.
+        deposit: This method contains logic to increment to the current account
+                 balance.
+        withdraw: This method contains logic to decrement to the current account
+                 balance.
+    """
+
+    def __init__(self):
+        """
+        Constructor that sets initial balance to the minimum amount of 500.
+        """
+        self.balance = 0
+
+    def deposit(self, amount):
+        """
+        Method to take in cash amounts and increment this to the current
+        balance.
+        """
+        if amount < 0:
+            return "Invalid amount"
+        else:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        """
+        Method to take in cash amounts and decrease this from the current
+        balance.
+        """
+        if amount < 0:
+            return "Invalid amount"
+        elif (self.balance - amount) < 0:
+            return "Cannot withdraw beyond the minimum balance"
+        else:
+            self.balance -= amount
