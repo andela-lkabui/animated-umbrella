9038e21cad4242e3ac8e07a353873d1d376511dd
Lewis Kabui <lewis.maina@andela.com>
2016-08-25 10:52:05 +0300

    (fix) Remove abstraction concept

--- bank_account.py
+++ bank_account.py
@@ -1,17 +1,12 @@
-from abc import ABCMeta, abstractmethod
-
-
 class BankAccount(object):
     """
-    Abstract BankAccount to be inherited by SavingsAccount and CurrentAccout.
+    BankAccount to be inherited by SavingsAccount and CurrentAccout.
     """
 
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
     def deposit(amount):
         """Takes in cash amount and increments it to the current balance."""
+        pass
 
-    @abstractmethod
     def withdraw(amount):
         """Takes in cash amount and decreases it from the current balance."""
+        pass
--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -3,44 +3,17 @@ import unittest
 from bank_account import BankAccount
 
 
-class TestBankAccount(unittest.TestCase):
-    """Class to test the BankAccount class"""
-
-    def test_that_bank_account_class_is_abstract(self):
-        """
-        Tests that BankAccount class is abstract and cannot be instantiated.
-        """
-        with self.assertRaises(TypeError):
-            BankAccount()
-        with self.assertRaisesRegexp(TypeError, "Can't instantiate abstract class BankAccount with abstract methods deposit, withdraw"):
-            BankAccount()
-
+class BankAccountTestCases(unittest.TestCase):
+  
     def test_that_bank_account_class_has_withdraw_method(self):
-        """
-        Tests that BankAccount class has a withdraw method.
-        """
         try:
             self.assertTrue(BankAccount.deposit)
         except AttributeError as ae:
-            self.assertFalse(
-                ae,
-                msg="Virtual method deposit has not been defined in BankAccount")
+            self.assertFalse(ae, msg="Virtual method deposit has not been defined in BankAccount")
 
-    def test_that_bank_account_abstract_class_has_deposit_virtual_method(self):
-        """
-        Tests that BankAccount class has a deposit method.
-        """
+    def test_that_bank_account_class_has_deposit_method(self):
         try:
             self.assertTrue(BankAccount.withdraw)
         except AttributeError as ae:
-            self.assertFalse(
-                ae,
-                msg="Virtual method withdraw has not been defined in BankAccount")
-
-    def test_that_withdraw_and_deposit_are_abstract_methods(self):
-        """
-        Tests that BankAccount class has abstract methods *deposit* and
-        *withdraw*
-        """
-        self.assertTrue('deposit' in BankAccount.__abstractmethods__)
-        self.assertTrue('withdraw' in BankAccount.__abstractmethods__)
+            self.assertFalse(ae, msg="Virtual method withdraw has not been defined in BankAccount")
+            
