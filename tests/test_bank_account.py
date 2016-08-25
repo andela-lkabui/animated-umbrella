3eb7fe4478d367e3d9a0b5037a1b6990edbc37c2
Lewis Kabui <lewis.maina@andela.com>
2016-08-25 17:13:11 +0300

    (fix) Display approproate feedback message

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -9,11 +9,10 @@ class BankAccountTestCases(unittest.TestCase):
         try:
             self.assertTrue(BankAccount.deposit)
         except AttributeError as ae:
-            self.assertFalse(ae, msg="Virtual method deposit has not been defined in BankAccount")
+            self.assertFalse(ae, msg="Method deposit has not been defined in BankAccount")
 
     def test_that_bank_account_class_has_deposit_method(self):
         try:
             self.assertTrue(BankAccount.withdraw)
         except AttributeError as ae:
-            self.assertFalse(ae, msg="Virtual method withdraw has not been defined in BankAccount")
-            
+            self.assertFalse(ae, msg="Method withdraw has not been defined in BankAccount")
